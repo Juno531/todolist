6ac340e616400ddb5ed5e178564942562aabf44e
Ji99 <rksk0531@naver.com>
2022-08-11 15:44:15 +0900

    commit

--- src/component/List.tsx
+++ src/component/List.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "./List.module.css";
 import { ReactComponent as DeleteIcon } from "../assets/icons/delete.svg";
+import { ReactComponent as AddIcon } from "../assets/icons/add.svg";
 
 interface ListProps {
   mykey: string;
@@ -11,41 +12,85 @@ type Data = {
   textData: string;
 };
 
-let data: string;
-let toDos: Data[] = [];
+let toDos: Data[] = []; // 임시 저장 배열
 
 function List(props: ListProps) {
   const TODOS_KEY = props.mykey;
+  const [text, setText] = useState("");
+  const [list, setList] = useState<Data[]>([]);
 
-  if (localStorage.getItem(TODOS_KEY) !== null)
-    data = localStorage.getItem(TODOS_KEY)!;
-  else data = "";
-  toDos = JSON.parse(localStorage.getItem(TODOS_KEY)!);
+  useEffect(() => {
+    getData();
+  });
 
-  const onDeleteClick = (event: any, key: number) => {
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setText(e.currentTarget.value);
+  };
+
+  const getData = () => {
+    const data: string = localStorage.getItem(TODOS_KEY)!;
+    setList(JSON.parse(data));
+  };
+
+  /* 
+  로컬스토리지에 Todo 저장
+  */
+  const onClick = () => {
+    const newToDoObj: Data = {
+      mykey: Date.now(),
+      textData: text,
+    };
+
+    if (text.length >= 1) {
+      toDos.push(newToDoObj);
+      setList(toDos);
+      console.log(typeof toDos);
+      localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+      setText("");
+    }
+
+    console.log(toDos);
+  };
+
+  // 로컬스토리지 데이터 삭제
+  const onDeleteClick = (event: any) => {
     const list = event.target.parentElement;
-    toDos = toDos.filter((toDo) => toDo.mykey !== key);
-    list.remove();
+    console.log(list.id);
+    toDos = toDos.filter((toDo) => `${toDo.mykey}` !== list.id);
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
   };
 
   return (
-    <div className={styled.listContainer}>
-      {data !== ""
-        ? JSON.parse(data).map((list: Data, index: number) => (
-            <div className={styled.todoItem} key={list.mykey}>
-              {list.textData}
-              <button
-                className={styled.listDeleteButton}
-                onClick={() => {
-                  onDeleteClick(event, list.mykey);
-                }}
-              >
-                <DeleteIcon className={styled.deleteIcon}></DeleteIcon>
-              </button>
-            </div>
-          ))
-        : ""}
+    <div>
+      <div className={styled.todoAdd}>
+        <input
+          className={styled.todoAddInput}
+          value={text}
+          onChange={onChange}
+          placeholder="오늘의 할일"
+          required={true}
+          minLength={2}
+          maxLength={50}
+        ></input>
+        <div className={styled.icon}>
+          <AddIcon className={styled.icon} onClick={onClick}></AddIcon>
+        </div>
+      </div>
+      <div className={styled.listContainer}>
+        {toDos !== []
+          ? toDos.map((list: Data, index: number) => (
+              <div id={`${list.mykey}`} className={styled.todoItem} key={index}>
+                {list.textData}
+                <button
+                  className={styled.listDeleteButton}
+                  onClick={onDeleteClick}
+                >
+                  <DeleteIcon className={styled.deleteIcon}></DeleteIcon>
+                </button>
+              </div>
+            ))
+          : ""}
+      </div>
     </div>
   );
 }
--- src/pages/Daylist.tsx
+++ src/pages/Daylist.tsx
@@ -1,70 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "./ListStyle.module.css";
 import TodayDate from "../component/TodayDate";
 import List from "../component/List";
-import { ReactComponent as AddIcon } from "../assets/icons/add.svg";
-
-type Data = {
-  mykey: number;
-  textData: string;
-};
 
 const TODOS_KEY = "daytodos"; // localStorage KEY
-const todos: Data[] = []; // input 데이터를 저장하기 위한 임시 배열
-
-function TodoItem({ mykey, textData }: Data) {
-  return (
-    <div key={mykey}>
-      <h2>{textData}</h2>
-    </div>
-  );
-}
 
 function Daylist() {
-  const [text, setText] = useState("");
-  const [list, setList] = useState<Data[]>([]);
-
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setText(e.currentTarget.value);
-  };
-
-  const onClick = () => {
-    const newToDoObj: Data = {
-      mykey: Date.now(),
-      textData: text,
-    };
-    if (text.length >= 1) {
-      todos.push(newToDoObj);
-      setList(todos);
-      console.log(typeof todos);
-      localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
-      setText("");
-    }
-
-    console.log(todos);
-  };
-
   return (
     <div className={styled.container}>
       <div>
         <TodayDate></TodayDate>
       </div>
-      <div className={styled.todoAdd}>
-        <input
-          className={styled.todoAddInput}
-          value={text}
-          onChange={onChange}
-          placeholder="오늘의 할일"
-          required={true}
-          minLength={2}
-          maxLength={50}
-        ></input>
-        <div className={styled.icon}>
-          <AddIcon className={styled.icon} onClick={onClick}></AddIcon>
-        </div>
-      </div>
       <div className={styled.todoContainer}>
-        <List mykey="daytodos"></List>
+        <List mykey={`${TODOS_KEY}`}></List>
       </div>
     </div>
   );
